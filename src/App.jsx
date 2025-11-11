75035a2e202de53833ae4cb270edf94f1dba54fc
Te3sk <fulgaro.niccolo@gmail.com>
2025-11-11 18:10:13 +0100

    fix some designs and fix results page

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function LoadingPage() {
       <div className='w-full h-full flex justify-center items-center'>
         <LoadingSpinner
           time={95000}
-          message={"temp message temp message temp message temp message temp message temp message temp message"}
+          message={"Stiamo cercando le migliori offerte per te..."}
         />
       </div>
     </div>
@@ -33,6 +33,135 @@ function App() {
   const [error, setError] = useState(null) // TODO: clean the error message sys
   const [loading, setLoading] = useState(false)
   const [results, setResults] = useState(null)
+  // // TODO: restore real resutls usestate (above)
+  // const [results, setResults] = useState([
+  //   {
+  //     "title": "1 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "2 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "3 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "4 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "5 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "6 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "7 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "8 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  //   {
+  //     "title": "9 - Appartamento All'asta In Via Generale V Streva, N 21 - 90044 Carini (PA)- LOTTO 22003",
+  //     "address": "via generale v streva, n 21 - 90044 Carini (PA)-Roma- Libertà - Villabianca",
+  //     "id": "EX8691189",
+  //     "auction_date": "04/12/2025",
+  //     "mq": null,
+  //     "rooms": "-",
+  //     "price": "4094",
+  //     "autonomous": false,
+  //     "bathroom": "-",
+  //     "energy class": "-",
+  //     "description": "Partecipa all'asta di un appartamento situato in una zona centrale di Palermo, presso il Tribunale della città. L'offerta minima per questo immobile è di soli 4094 €. Non perdere l'opportunità di fare un ottimo affare e di aggiudicarti questa proprietà.L'appartamento è situato al piano terzo di un edificio a Palermo, in via Generale Vincenzo Streva 21. Si tratta di un appartamento di cui si possiede 1/6 della proprietà. Altre informazioni specifiche sull'appartamento non sono disponibili.In pochi minuti si possono raggiungere i principali servizi tra i quali supermercato (1.04 km), poste (0.31 km), scuola (0.79 km), banca (0.24 km), bancomat (1.09 km), bar (0.29 km), fermata autobus (3.21 km), siti storici (0.72 km).",
+  //     "url": "https://www.immobiliallasta.it/immobili/roma/roma/ex8691189/appartamento-all-asta-in-via-generale-v-streva"
+  //   },
+  // ])
 
   if (error && typeof error === 'object') {
     return (
